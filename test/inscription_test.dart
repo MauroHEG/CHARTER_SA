62cb32b913dd267f3662f05e66fb7ed35b2681d7
MauroHEG <mauro.vicario@etu.hesge.ch>
2023-04-11 16:41:02 +0200

    Avancement tests inscription

--- test/inscription_test.dart
+++ test/inscription_test.dart
@@ -1,35 +1,69 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
 import 'package:test/test.dart';
 import 'package:charter_appli_travaux_mro/view/signup_screen.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 //Test unitaire pour la fonctionnalitée "Inscription" --> [signup_screen.dart]
-void main() {
+void main() async {
   //setUp() est appelée avant chaque test (nettoyer bdd)
-  setUp(() {
+  /*setUp(() {
     //Code nettoyer bdd
-  });
+  });*/
+  //Initialisation de la cloud firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   group('Utilisateur unique', () {
-    //Création d'un utilisateur inexistant
-    test('utilisateur unique', () async {
-      final instance = FakeFirebaseFirestore();
-      /*try {
-        UserCredential userCredential = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(
-                email: "elvio.vic@gmail.com", password: "mdp123!test");
-        await FirebaseFirestore.instance
-            .collection('utilisateurs')
-            .doc(userCredential.user!.uid)
-            .set({
-          'prenom': "elvio",
-          'nom': "Dupont",
-          'email': "elvio.vic@gmail.com",
-          'telephone': "0779415789",
-          'role': 'user'
-        });
-      } catch (e) {}*/
+    //Création d'un utilisateur inexistant sur une FakeFirebase (bdd pour les tests)
+    test('test fonctionnalitée : _enregistrerUtilisateur()', () async {
+      // Créer une instance de FakeFirebaseFirestore
+      final fakeFirestore = FakeFirebaseFirestore();
+
+      // Créer une instance de FirebaseAuth avec un utilisateur enregistré
+      final userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: 'elvio.vic@gmail.com', password: 'mdp123!test');
+
+      await FirebaseFirestore.instance
+          .collection('utilisateurs')
+          .doc(userCredential.user!.uid)
+          .set({
+        'prenom': "elvio",
+        'nom': "Dupont",
+        'email': "elvio.vic@gmail.com",
+        'telephone': "0779415789",
+        'role': 'user'
+      });
+
+      // Vérifier que l'utilisateur a été enregistré dans Firestore
+      final snapshot = await fakeFirestore
+          .collection('utilisateurs')
+          .doc(userCredential.user!.uid)
+          .get();
+      expect(snapshot.exists, isTrue);
+      expect(snapshot.data()!['prenom'], equals('elvio'));
+      expect(snapshot.data()!['nom'], equals('Dupont'));
+      expect(snapshot.data()!['email'], equals('elvio.vic@gmail.com'));
+      expect(snapshot.data()!['telephone'], equals('0779415789'));
+      expect(snapshot.data()!['role'], equals('user'));
+      /*final instance = FakeFirebaseFirestore();
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: "elvio.vic@gmail.com", password: "mdp123!test");
+      await FirebaseFirestore.instance
+          .collection('utilisateurs')
+          .doc(userCredential.user!.uid)
+          .set({
+        'prenom': "elvio",
+        'nom': "Dupont",
+        'email': "elvio.vic@gmail.com",
+        'telephone': "0779415789",
+        'role': 'user'
+      });*/
     });
 
     //Création du même utilisateur (doit générer une erreur)
