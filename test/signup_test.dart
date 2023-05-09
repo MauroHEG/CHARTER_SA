b564bcb6b3ffd46e57d7a82e0720eefff6426a74
MauroHEG <mauro.vicario@etu.hesge.ch>
2023-05-09 16:31:44 +0200

    Avancement test

--- test/signup_test.dart
+++ test/signup_test.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:charter_appli_travaux_mro/view/services/auth_service.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -41,6 +43,32 @@ void main() {
       expect(userDoc['email'], email);
       expect(userDoc['telephone'], phoneNumber);
       expect(userDoc['role'], 'user');
+    });
+
+    test('Un utilisateur est unique (pas deux fois  la même adresse mail)',
+        () async {
+      // Les données pour l'inscription
+      const email = 'testDoublon@test.com';
+      const password = 'motdepasse';
+      const firstName = 'Double';
+      const lastName = 'Doubleur';
+      const phoneNumber = '0778547895';
+
+      await authService.enregistrerUtilisateur(
+          email, password, firstName, lastName, phoneNumber);
+
+      /*final user1 = auth.currentUser!;
+
+      await authService.enregistrerUtilisateur(
+          email, "motdepasse19", "Double2", "Doubleur2", "07777777");
+
+      final userDouble = auth.currentUser!;
+
+      expect(user1['email'], notEqual(first, second))*/
+      expect(
+          authService.enregistrerUtilisateur(
+              email, password, firstName, lastName, phoneNumber),
+          throw Exception('Le compte existe déjà pour cet e-mail.'));
     });
   });
 }
