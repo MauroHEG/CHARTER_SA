2efc4cb131ccb4dfd82deb9f56dcb13c4067a3d8
aleksabrtc <bratic.aleksa@gmail.com>
2023-05-11 12:28:30 +0200

    màj res

--- lib/web_admin/screens/reservations_list_page.dart
+++ lib/web_admin/screens/reservations_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:charter_appli_travaux_mro/web_admin/screens/reservation_form_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ReservationsListPage extends StatefulWidget {
   const ReservationsListPage({super.key});
@@ -35,7 +36,7 @@ class _ReservationsListPageState extends State<ReservationsListPage> {
             // Affichage de la liste des réservations
             return ListView(
               children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
+                Map<String, dynamic> donnees =
                     document.data() as Map<String, dynamic>;
                 return InkWell(
                   onTap: () {
@@ -44,14 +45,60 @@ class _ReservationsListPageState extends State<ReservationsListPage> {
                       context,
                       MaterialPageRoute(
                         builder: (context) => ReservationDetailPage(
-                          reservationData: data,
+                          reservationData: donnees,
                         ),
                       ),
                     );
                   },
                   child: ListTile(
-                    title: Text(data['nomPays']),
-                    subtitle: Text(data['utilisateur']),
+                    title: Text(donnees['nomPays']),
+                    subtitle: FutureBuilder<String>(
+                      future: getUserName(donnees['utilisateur']),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<String> snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(snapshot.data!);
+                        } else if (snapshot.hasError) {
+                          return Text('Erreur: ${snapshot.error}');
+                        } else {
+                          return CircularProgressIndicator();
+                        }
+                      },
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Confirmer la suppression'),
+                              content: const Text(
+                                  'Voulez-vous vraiment supprimer cette réservation ?'),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: const Text('ANNULER'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                TextButton(
+                                  child: const Text('CONFIRMER'),
+                                  onPressed: () {
+                                    // Supprimer la réservation de la base de données
+                                    FirebaseFirestore.instance
+                                        .collection('reservations')
+                                        .doc(document.id)
+                                        .delete();
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    ),
                   ),
                 );
               }).toList(),
@@ -72,6 +119,14 @@ class _ReservationsListPageState extends State<ReservationsListPage> {
           child: const Icon(Icons.add),
         ));
   }
+
+  Future<String> getUserName(String userId) async {
+    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+        .collection('utilisateurs')
+        .doc(userId)
+        .get();
+    return (userSnapshot.data() as Map<String, dynamic>)['nom'];
+  }
 }
 
 class ReservationDetailPage extends StatefulWidget {
@@ -85,11 +140,14 @@ class ReservationDetailPage extends StatefulWidget {
 
 class _ReservationDetailPageState extends State<ReservationDetailPage> {
   String? _userName;
+  String? _formattedDateDebut;
+  String? _formattedDateFin;
 
   @override
   void initState() {
     super.initState();
     fetchUserName();
+    formatDateDebutFin();
   }
 
   Future<void> fetchUserName() async {
@@ -101,6 +159,16 @@ class _ReservationDetailPageState extends State<ReservationDetailPage> {
     setState(() {
       _userName = (userSnapshot.data() as Map<String, dynamic>)['nom'];
     });
+  }
+
+  void formatDateDebutFin() {
+    DateTime dateDebut =
+        (widget.reservationData['dateDebut'] as Timestamp).toDate();
+    DateTime dateFin =
+        (widget.reservationData['dateFin'] as Timestamp).toDate();
+
+    _formattedDateDebut = DateFormat('dd/MM/yyyy').format(dateDebut);
+    _formattedDateFin = DateFormat('dd/MM/yyyy').format(dateFin);
   }
 
   @override
@@ -132,6 +200,10 @@ class _ReservationDetailPageState extends State<ReservationDetailPage> {
                       Text('Nom du pays: ${widget.reservationData['nomPays']}'),
                       const SizedBox(height: 5),
                       Text('Utilisateur: ${_userName ?? 'Chargement...'}'),
+                      const SizedBox(height: 5),
+                      Text('Date de début: $_formattedDateDebut'),
+                      const SizedBox(height: 5),
+                      Text('Date de fin: $_formattedDateFin'),
                       const SizedBox(height: 5),
                       Text(
                           "Nom de l'hôtel: ${widget.reservationData['nomHotel']}"),
