cfe6f03ec4623005ba78d3114ac0f29c2cc11306
aleksabrtc <bratic.aleksa@gmail.com>
2023-05-17 12:54:45 +0200

    m

--- lib/view/services/reservation_service.dart
+++ lib/view/services/reservation_service.dart
@@ -1,6 +1,10 @@
+import 'package:charter_appli_travaux_mro/view/services/user_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class ReservationService {
+  final UserService _userService = UserService();
+
   final CollectionReference reservations =
       FirebaseFirestore.instance.collection('reservations');
 
@@ -10,5 +14,15 @@ class ReservationService {
 
   Stream<QuerySnapshot> getReservations() {
     return reservations.snapshots();
+  }
+
+  Future<String> fetchUserName(Map<String, dynamic> reservationData) async {
+    String userId = reservationData['utilisateur'];
+    return await _userService.getUserName(userId);
+  }
+
+  String formatDate(Map<String, dynamic> reservationData, String dateType) {
+    DateTime date = (reservationData[dateType] as Timestamp).toDate();
+    return DateFormat('dd/MM/yyyy').format(date);
   }
 }
