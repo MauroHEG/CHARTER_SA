8d37d58d0b3b1c46aecc8067cc414c7f724ab89f
aleksabrtc <bratic.aleksa@gmail.com>
2023-05-17 12:25:18 +0200

    m

--- lib/view/reservation_details_screen.dart
+++ lib/view/reservation_details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// Importez le package des icônes Font Awesome
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
