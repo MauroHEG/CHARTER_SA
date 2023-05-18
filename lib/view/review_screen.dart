cad47af66ebf1fdefcdf9c709daa824db64e4744
aleksabrtc <bratic.aleksa@gmail.com>
2023-05-18 21:45:23 +0200

    màj avis

--- lib/view/review_screen.dart
+++ lib/view/review_screen.dart
@@ -63,121 +63,142 @@ class _ReviewScreenState extends State<ReviewScreen> {
       appBar: AppBar(
         title: Text('Publier un avis'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                Text('Destination: ${widget.reservationDetails['nomPays']}'),
-                Text('Ville: ${widget.reservationDetails['nomVille']}'),
-                TextFormField(
-                  controller: _titreController,
-                  decoration: InputDecoration(
-                    labelText: 'Titre',
+      body: Center(
+        // Center content vertically
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center content horizontally
+                children: <Widget>[
+                  Text(
+                    '${widget.reservationDetails['nomPays']} - ${widget.reservationDetails['nomVille']}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Veuillez entrer un titre pour l\'avis.';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _avisController,
-                  decoration: InputDecoration(
-                    labelText: 'Avis',
+                  SizedBox(height: 16.0),
+                  Card(
+                    // Using card for better UI
+                    child: TextFormField(
+                      controller: _titreController,
+                      decoration: InputDecoration(
+                        labelText: 'Titre',
+                        contentPadding: EdgeInsets.all(
+                            10.0), // Padding inside TextFormField
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer un titre pour l\'avis.';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                  maxLines: 5,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Veuillez entrer un avis.';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                RatingBar.builder(
-                  initialRating: 3,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: Colors.amber,
+                  SizedBox(height: 16.0),
+                  Card(
+                    // Using card for better UI
+                    child: TextFormField(
+                      controller: _avisController,
+                      decoration: InputDecoration(
+                        labelText: 'Avis',
+                        contentPadding: EdgeInsets.all(
+                            10.0), // Padding inside TextFormField
+                      ),
+                      maxLines: 5,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer un avis.';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                  onRatingUpdate: (rating) {
-                    _rating = rating;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: _images.length + 1,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
+                  SizedBox(height: 16.0),
+                  RatingBar.builder(
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      _rating = rating;
+                    },
                   ),
-                  itemBuilder: (context, index) {
-                    if (index == _images.length) {
-                      return IconButton(
-                        icon: Icon(Icons.add_a_photo),
-                        onPressed: _pickImage,
-                      );
-                    }
-                    return Image.file(_images[index]);
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      await _uploadImages();
-                      Avis avis = Avis(
-                        destination: widget.reservationDetails['nomPays'],
-                        ville: widget.reservationDetails['nomVille'],
-                        titre: _titreController.text,
-                        avis: _avisController.text,
-                        note: _rating.round(),
-                        images: _imageUrls,
-                      );
+                  SizedBox(height: 16.0),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    itemCount: _images.length + 1,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      if (index == _images.length) {
+                        return IconButton(
+                          icon: Icon(Icons.add_a_photo),
+                          onPressed: _pickImage,
+                        );
+                      }
+                      return Image.file(_images[index]);
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        await _uploadImages();
+                        Avis avis = Avis(
+                          destination: widget.reservationDetails['nomPays'],
+                          ville: widget.reservationDetails['nomVille'],
+                          titre: _titreController.text,
+                          avis: _avisController.text,
+                          note: _rating.round(),
+                          images: _imageUrls,
+                        );
 
-                      FirebaseFirestore.instance.collection('avis').add({
-                        'userId': widget.userId,
-                        'destination': avis.destination,
-                        'ville': avis.ville,
-                        'titre': avis.titre,
-                        'avis': avis.avis,
-                        'note': avis.note,
-                        'images': avis.images,
-                      });
+                        FirebaseFirestore.instance.collection('avis').add({
+                          'userId': widget.userId,
+                          'destination': avis.destination,
+                          'ville': avis.ville,
+                          'titre': avis.titre,
+                          'avis': avis.avis,
+                          'note': avis.note,
+                          'images': avis.images,
+                        });
 
-                      FirebaseFirestore.instance
-                          .collection('utilisateurs')
-                          .doc(widget.userId)
-                          .collection('avis')
-                          .add({
-                        'destination': avis.destination,
-                        'ville': avis.ville,
-                        'titre': avis.titre,
-                        'avis': avis.avis,
-                        'note': avis.note,
-                        'images': avis.images,
-                      });
+                        FirebaseFirestore.instance
+                            .collection('utilisateurs')
+                            .doc(widget.userId)
+                            .collection('avis')
+                            .add({
+                          'destination': avis.destination,
+                          'ville': avis.ville,
+                          'titre': avis.titre,
+                          'avis': avis.avis,
+                          'note': avis.note,
+                          'images': avis.images,
+                        });
 
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text('Avis enregistré avec succès!')));
-                      Navigator.of(context).pop();
-                    }
-                  },
-                  child: Text('Publier l\'avis'),
-                ),
-              ],
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('Avis enregistré avec succès!')));
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: Text('Publier l\'avis'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
